cca96ef4c72b6eeedaead7e568e3b85231ddc31a
casoblus <casobrinolusquinos@gmail.com>
2016-04-06 19:55:16 +0200

    Empregado >> inicio

---
+++ Empregado/Empregado.java
@@ -0,0 +1,23 @@
+public class Empregado
+{
+	private String dni;
+	private String nome;
+	private String apelidos;
+	private float salario;
+	private static int num_emp = 0;
+
+	public void engadir()
+	{}
+
+	public void eliminar()
+	{}
+
+	public void modificar()
+	{}
+
+	public void visualizar()
+	{}
+
+	public void calcular_salario()
+	{}
+}
